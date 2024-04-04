b0f39f7b109b01d8493788b4b7de7e341475d371
Dominik Domagalski <dragenetdev@gmail.com>
2024-04-04 21:05:20 +0200

    Initial commit

---
+++ apps/translations-manager-service/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'translations-manager-service',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/translations-manager-service'
+};
