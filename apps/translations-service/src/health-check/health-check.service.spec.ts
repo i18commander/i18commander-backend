08d844824a95a33f9d8f48d0b3214b61bbc75aa4
Dominik Domagalski <dragenetdev@gmail.com>
2024-04-09 22:43:38 +0200

    Add service-check test

---
+++ apps/translations-service/src/health-check/health-check.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthCheckService } from './health-check.service';
+
+describe('HealthCheckService', () => {
+  let service: HealthCheckService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [HealthCheckService],
+    }).compile();
+
+    service = module.get<HealthCheckService>(HealthCheckService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('getHealthzStatus', () => {
+    it('should return status - healthy', () => {
+      expect(service.getHealthzStatus()).toEqual({ status: 'healthy' });
+    });
+  });
+});
