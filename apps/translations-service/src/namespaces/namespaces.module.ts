a290365fc6eac093b8614df5a2b4c5105212fa29
Dominik Domagalski <dragenetdev@gmail.com>
2024-05-10 17:02:52 +0200

    ICO-5: Add empty namespaces module

---
+++ apps/translations-service/src/namespaces/namespaces.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { NamespacesController } from './namespaces.controller';
+import { NamespacesService } from './namespaces.service';
+
+@Module({
+  controllers: [NamespacesController],
+  providers: [NamespacesService]
+})
+export class NamespacesModule {}
